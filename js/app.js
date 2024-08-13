c5c40525d3d9ddf755c59324a91077856140f8da
jwg23417 <maximgereta@gmail.com>
2024-08-13 18:42:29 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const cards = document.querySelector('.cards')
 const welcomeSub = document.querySelector('.welcome-sub') // Додаємо посилання на елемент welcome-sub
 
 startBtn.addEventListener('click', function () {
-	cards.classList.toggle('none')
+	cards.classList.remove('none')
+	startBtn.classList.add('none')
 	welcomeSub.textContent = 'Choose your first job' // Змінюємо текст елемента welcome-sub
 })
