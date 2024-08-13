6771d6a3c530b5c8617aa67b39c85f6b9833a836
jwg23417 <maximgereta@gmail.com>
2024-08-13 18:46:24 +0300

    Delete js/app.js

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const startBtn = document.querySelector('.welcome')
-const cards = document.querySelector('.cards')
-const welcomeSub = document.querySelector('.welcome-sub') // Додаємо посилання на елемент welcome-sub
-
-startBtn.addEventListener('click', function () {
-	cards.classList.remove('none')
-	startBtn.classList.add('none')
-	welcomeSub.textContent = 'Choose your first job' // Змінюємо текст елемента welcome-sub
-})
